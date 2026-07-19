f099f13f98c0dd2119d33d7810d19cff0c63e1af
Dan Rojas <s3529602@students.fscj.edu>
2026-07-18 21:43:57 -0400

    Add Module 9 with PenguinOutput.txt

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,6 +1,6 @@
 // PalmerPenguins.java
-// D. Singletary
-// 2/22/25
+// Dan Rojas
+// 7/18/26
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 package edu.fscj.cop2800c.penguin;
@@ -8,7 +8,15 @@ package edu.fscj.cop2800c.penguin;
 public class PalmerPenguins {
     public static void main(String[] args) {
         // Create an instance of PenguinAnalyzer
+        PenguinAnalyzer analyzer = new PenguinAnalyzer();
 
+        // Read CSV data
+        analyzer.readPenguinData();
 
+        // Print formatted output
+        analyzer.printPenguinData();
+
+        // Save results to a file
+        analyzer.writePenguinData();
     }
 }
